ac4c07c6bc2030971654a1e8c163ded2f237b886
DidenkoSveta <106669577+DidenkoSveta@users.noreply.github.com>
2022-10-10 14:26:42 +0400

    started to perform tasks

---
+++ 12-create-object.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const myPerson = {
+    name: 'Svetlana',
+    surname: 'Didenko',
+    favoriteNumber: 7
+}
+
+console.log('My name is ' + myPerson.name + ' ' + myPerson.surname + ' and my favorite number is ' + myPerson.favoriteNumber)
