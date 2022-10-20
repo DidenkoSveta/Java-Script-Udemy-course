9756e2e7ca4d10ec088e96df45595931985b97a8
DidenkoSveta <106669577+DidenkoSveta@users.noreply.github.com>
2022-10-21 02:25:25 +0400

    upload solution tasks

---
+++ 28-let.js
@@ -0,0 +1,28 @@
+/** ЗАДАЧА 28 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+
+// const myFavoriteAnimal = 'Monkey';
+
+// console.log(myFavoriteAnimal);
+// // 'Monkey'
+
+// const myFavoriteAnimal = 'Cat';
+
+// console.log(myFavoriteAnimal);
+// // 'Cat' 
+
+
+//Константу нельзя изменять, это постоянная переменная
+//Переименовываем константу Cat и код работает
+
+const myFavoriteAnimal = 'Monkey';
+
+console.log(myFavoriteAnimal);
+// 'Monkey'
+
+const myFavoriteAnimal2 = 'Cat';
+
+console.log(myFavoriteAnimal2);
+// 'Cat'
